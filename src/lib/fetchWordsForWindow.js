06c02fb82f51341b31984db8570cdaa6318bb2de
Brian <brianflaherty64@gmail.com>
2021-12-02 11:18:44 -0500

    fix: fetchWordsForWindow now properly fetches words

--- src/lib/fetchWordsForWindow.js
+++ src/lib/fetchWordsForWindow.js
@@ -1,13 +1,13 @@
 const get = require('lodash/get');
-const map = require('rxjs/operators');
+const {map} = require('rxjs/operators');
 
 const {client} = require('@buccaneerai/graphql-sdk');
 
-const fetchWordsForWindow = (
+const fetchWordsForWindow = ({
   _gql = client,
   token = process.env.JWT_TOKEN,
   url = process.env.GRAPHQL_URL
-) => ({noteWindowId}) => {
+} = {}) => ({noteWindowId}) => {
   const words$ = _gql({url, token}).findWords({filter: {noteWindowId}}).pipe(
     map(response => get(response, 'words', []))
   );
